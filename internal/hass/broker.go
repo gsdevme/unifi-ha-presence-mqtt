f6e6084efadda08829418414037067a7e9a34644
Gavin Staniforth <gsdevme@users.noreply.github.com>
2021-10-15 00:24:13 +0100

    fix: ensure broker tcp is open and implement timeouts

--- internal/hass/broker.go
+++ internal/hass/broker.go
@@ -7,39 +7,98 @@ import (
 	"github.com/gsdevme/unifi/pkg/unifi"
 	"regexp"
 	"strings"
+	"time"
 )
 
 const Home = "home"
 const NotHome = "not_home"
 
-func PublishClientState(client unifi.ClientResponse, broker mqtt.Client) {
+func ensureBrokerOpen(broker mqtt.Client) error {
+	if !broker.IsConnectionOpen() {
+		t := broker.Connect()
+
+		// TODO sort this out better
+		for !t.WaitTimeout(3 * time.Second) {
+
+		}
+
+		if err := t.Error(); err != nil {
+			return fmt.Errorf("cannot connect to broker: %s", err)
+		}
+	}
+
+	return nil
+}
+
+func PublishClientState(client unifi.ClientResponse, broker mqtt.Client) error {
+	if err := ensureBrokerOpen(broker); err != nil {
+		return err
+	}
+
 	device, _ := json.Marshal(DeviceAttributes{
 		IpAddress:  client.IpAddress,
 		MacAddress: client.MacAddress,
 	})
 
-	broker.Publish(getStateTopic(client.Hostname), 0, false, Home)
-	broker.Publish(getAttributesTopic(client.Hostname), 0, false, string(device))
+	var t mqtt.Token
+
+	t = broker.Publish(getStateTopic(client.Hostname), 0, false, Home)
+
+	if !t.WaitTimeout(time.Second * 1000) {
+		return fmt.Errorf("failed to publish home %w", t.Error())
+	}
+
+	t = broker.Publish(getAttributesTopic(client.Hostname), 0, false, string(device))
+
+	if !t.WaitTimeout(time.Second * 1000) {
+		return fmt.Errorf("failed to publish attributes %w", t.Error())
+	}
+
+	return nil
 }
 
-func PublishNotHome(name string, broker mqtt.Client) {
-	broker.Publish(getStateTopic(name), 0, false, NotHome)
+func PublishNotHome(name string, broker mqtt.Client) error {
+	if err := ensureBrokerOpen(broker); err != nil {
+		return err
+	}
+
+	t := broker.Publish(getStateTopic(name), 0, false, NotHome)
+
+	if !t.WaitTimeout(time.Second * 1000) {
+		return fmt.Errorf("failed to publish not_home %w", t.Error())
+	}
+
+	return nil
 }
 
-func PublishAutoDiscoveryForClient(client unifi.ClientResponse, broker mqtt.Client) {
+func PublishAutoDiscoveryForClient(client unifi.ClientResponse, broker mqtt.Client) error {
+	if err := ensureBrokerOpen(broker); err != nil {
+		return err
+	}
+
 	config := newDiscoveryConfig(client.Hostname, client.DeviceName)
 	payload, _ := json.Marshal(config)
 
-	broker.Publish(fmt.Sprintf("homeassistant/device_tracker/%s/config", config.UniqueId), 0, false, string(payload))
+	t := broker.Publish(fmt.Sprintf("homeassistant/device_tracker/%s/config", config.UniqueId), 0, false, string(payload))
 
-	PublishClientState(client, broker)
+	if !t.WaitTimeout(time.Second * 1000) {
+		return fmt.Errorf("failed to publish auto discovery %w", t.Error())
+	}
+
+	return PublishClientState(client, broker)
 }
 
-func PublishAutoDiscoveryPlaceholder(name string, broker mqtt.Client) {
+func PublishAutoDiscoveryPlaceholder(name string, broker mqtt.Client) error {
 	config := newDiscoveryConfig(name, "Unknown")
 	payload, _ := json.Marshal(config)
 
-	broker.Publish(fmt.Sprintf("homeassistant/device_tracker/%s/config", config.UniqueId), 0, false, string(payload))
+	t := broker.Publish(fmt.Sprintf("homeassistant/device_tracker/%s/config", config.UniqueId), 0, false, string(payload))
+
+	if !t.WaitTimeout(time.Second * 1000) {
+		return fmt.Errorf("failed to publish auto discovery %w", t.Error())
+	}
+
+	return nil
 }
 
 func newDiscoveryConfig(name string, manufacturer string) *discoveryConfig {
